50973fcef8b0ef3704b3d5664044ea2cdafbf4a0
Dennis <perk430@gmail.com>
2017-04-11 13:12:50 -0400

    Initial commit

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input from user
+
+            Console.WriteLine("Enter an integer between");
+            
+        }
+    }
+}
