843e8fd0d88f32449fa6d3527c081df6ebd53a37
Dennis <perk430@gmail.com>
2017-04-11 14:01:30 -0400

    Determining if input is even and within the range of 2 and 25

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,8 +11,22 @@ namespace Lab2
         static void Main(string[] args)
         {
             // Input from user
+            int num;
+            bool inRange;
 
-            Console.WriteLine("Enter an integer between");
+            Console.WriteLine("Enter an integer between 1 and 100");
+            num = int.Parse(Console.ReadLine());
+            // Determining whether input is ODD or Even
+            if (num % 2 == 1)
+            {
+                Console.WriteLine(num + "ODD");
+
+            }
+            else if (num % 2 == 0) 
+            inRange = (2 <= num) && (num <= 25);
+            {
+                Console.WriteLine("Even and less than 25");
+            }
             
         }
     }
